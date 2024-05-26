27d2be8d6a1624e3f280b6b838d21d46a51fa951
alnazareth <36893691+alnazareth@users.noreply.github.com>
2024-05-25 20:46:25 -0400

    initial update

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int num;
+Console.WriteLine("Escribe numero");
+num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(IsPrime(num));
+
+ static bool IsPrime(int num){
+
+for (int i = 2; i < num; i++)
+{
+    if (num% i ==0) return false;
+    
+}
+
+return true;
+}
+
+
+
+Console.WriteLine("El numero ingresado es:"+ num);
